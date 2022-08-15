3ef52d92763d78c785a68d073a3209c148c0dfee
damlacim <cim.damla@gmail.com>
2022-08-15 15:55:23 +0300

    solve day1

--- SwiftPlayground.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -1,3 +1,22 @@
-import Foundation
+import UIKit
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+// Declare second integer, double, and String variables.
+var int: Int
+var double: Double
+var string: String
+// Read and save an integer, double, and String to your variables.
+int = Int(readLine()!)!
+double = Double(readLine()!)!
+string = readLine()!
+// Print the sum of both integer variables on a new line.
+print(i + int)
+// Print the sum of the double variables on a new line.
+print(d + double)
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print("\(s)\(string)")
 
 
