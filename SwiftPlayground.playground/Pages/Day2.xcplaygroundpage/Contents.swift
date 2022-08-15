e287e80744a01eecee7d95d8440ded5fc8319398
damlacim <cim.damla@gmail.com>
2022-08-15 16:09:41 +0300

    solve day2

--- SwiftPlayground.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -1,2 +1,31 @@
-import UIKit
+import Foundation
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+func solve(meal_cost: Double, tip_percent: Int, tax_percent: Int) -> Void {
+    // Write your code here
+    let tip = meal_cost * Double(tip_percent) / 100
+    let tax = meal_cost * Double(tax_percent) / 100
+    let total = meal_cost + tax + tip
+    
+    print(Int(round(total)))
+}
+
+guard let meal_cost = Double((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let tip_percent = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let tax_percent = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+solve(meal_cost: meal_cost, tip_percent: tip_percent, tax_percent: tax_percent)
 
