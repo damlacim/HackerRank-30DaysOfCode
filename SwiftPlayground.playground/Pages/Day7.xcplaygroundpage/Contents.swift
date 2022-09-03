deadf1901fce7ebf484fd74b6a1b039ee6c2426c
damlacim <cim.damla@gmail.com>
2022-09-03 15:12:41 +0300

    solve day 7

---
+++ SwiftPlayground.playground/Pages/Day7.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+var reverse: [Int] = arr.reversed()
+for element in reverse {
+    print(element, terminator:" ")
+}
